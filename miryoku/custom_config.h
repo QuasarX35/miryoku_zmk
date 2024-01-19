d51851e47be19328096931c47cfad0be40fb44e8
QuasarX35 <shootingquasar555@gmail.com>
2024-01-20 01:19:53 +0800

    testing and adding genshin layer 16

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -26,6 +26,8 @@ MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun") \
 MIRYOKU_X(GAME, "Genshin")
 
+#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+
 #define U_BASE   0
 #define U_EXTRA  1
 #define U_TAP    2
@@ -38,4 +40,18 @@ MIRYOKU_X(GAME, "Genshin")
 #define U_FUN    9
 #define U_GAME   10
 
-#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+#if defined (MIRYOKU_KEYBOARD_KLEIN)
+#define XXX &none
+
+#define MIRYOKU_LAYERMAPPING_BASE( \
+K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
+) \
+K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09 \
+K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19 \
+K20  K21  K22  K23  K24                 K25  K26  K27  K28  K29 \
+          K32  K33  K34  XXX       XXX  K35  K36  K37
+
+#endif
