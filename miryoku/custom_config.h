2e855676da6252a14efb7fa7abb171adfc7c8670
QuasarX35 <shootingquasar555@gmail.com>
2024-01-20 13:58:26 +0800

    testing and adding genshin layer 25

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,12 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-// #define MIRYOKU_LAYER_BASE \
-// &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-// U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
-// U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-// U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
-
 #define MIRYOKU_LAYER_GENSHIN \
 &kp Q,             &kp W,             &kp A,             &kp S,             &kp D,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
@@ -44,7 +38,7 @@ MIRYOKU_X(GENSHIN, "Genshin")
 
 #define XXX &none
 
-#define MIRYOKU_LAYERMAPPING_BASE( \
+#define MIRYOKU_LAYERMAPPING_NUM( \
 K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
 K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
 K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
