980fc431a2117f3a0ae7170800896a5591764384
QuasarX35 <shootingquasar555@gmail.com>
2024-01-20 14:23:37 +0800

    testing and adding genshin layer 33

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,10 +2,10 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_LAYER_GENSHIN \
-&kp Q,             &kp W,             &kp A,             &kp S,             &kp D,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
+&kp Q,             &kp W,             &kp A,             &kp S,             &kp D,             &kp Y,             &kp U,             &kp I,             &kp O,             U_NU,                 \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),      \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_BASE, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_BASE, DEL),  U_NP,             U_NP
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
